7c2b4db5ea060c7139197ae394c6eb23df195863
abbyd0208 <abbyd0208@gmail.com>
2020-09-22 18:33:59 +0800

    新增 dashboard 並解把 navbar 與 sidebar拆開

--- vuemysite/src/router/index.js
+++ vuemysite/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/pages/Login'
+import Dashboard from '@/components/Dashboard'
+
 
 Vue.use(Router)
 
@@ -17,6 +19,12 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+     
     }
   ]
 })
